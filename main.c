2c26c362ed970cd4105cdd6f5a42cac3cc55f7cd
Lando, Gabriel <gabriel.lando@hp.com>
2020-08-26 12:30:16 -0300

    Changed main

--- main.c
+++ main.c
@@ -15,10 +15,10 @@ int main(int argc, char ** argv) {
     int answar = 0;
     int nota = 0;
     int i = 1;
-    fprintf(stderr, "Rodando main do prof. \n");
+    fprintf(stderr, "Rodando main que nao eh do prof. \n");
 
-    if (argc < 3) {
-        printf("call: ./etapa1 input.txt output.txt \n");
+    if (argc < 2) {
+        printf("call: ./etapa1 input.txt\n");
         exit(1);
     }
     if (0 == (yyin = fopen(argv[1], "r"))) {
@@ -30,33 +30,32 @@ int main(int argc, char ** argv) {
 
         if (!isRunning())
             break;
-
-        fprintf(stderr, "%d (%s) ", i, yytext);
+            
         switch(token) {
-            case KW_CHAR: printf("Char\n"); break;
-            case KW_INT: printf("Int\n"); break;
-            case KW_FLOAT: printf("Float\n"); break;
-            case KW_BOOL: printf("Bool\n"); break;
-            case KW_IF: printf("If\n"); break;
-            case KW_THEN: printf("Then\n"); break;
-            case KW_ELSE: printf("Else\n"); break;
-            case KW_WHILE: printf("While\n"); break;
-            case KW_LOOP: printf("Loop\n"); break;
-            case KW_READ: printf("Read\n"); break;
-            case KW_PRINT: printf("Print\n"); break;
-            case KW_RETURN: printf("Return\n"); break;
-            case OPERATOR_LE: printf("OPERATOR_LE\n"); break;
-            case OPERATOR_GE: printf("OPERATOR_GE\n"); break;
-            case OPERATOR_EQ: printf("OPERATOR_EQ\n"); break;
-            case OPERATOR_DIF: printf("OPERATOR_DIF\n"); break;
-            case TK_IDENTIFIER: printf("TK_IDENTIFIER\n"); break;
-            case LIT_INTEGER: printf("LIT_INTEGER\n"); break;
-            case LIT_FLOAT: printf("LIT_FLOAT\n"); break;
-            case LIT_TRUE: printf("LIT_TRUE\n"); break;
-            case LIT_FALSE: printf("LIT_FALSE\n"); break;
-            case LIT_CHAR: printf("LIT_CHAR\n"); break;
-            case LIT_STRING: printf("LIT_STRING\n"); break;
-            case TOKEN_ERROR: printf("TOKEN_ERROR\n"); break;
+            case KW_CHAR: printf("KW_CHAR (%s)\n", yytext); break;
+            case KW_INT: printf("KW_INT (%s)\n", yytext); break;
+            case KW_FLOAT: printf("KW_FLOAT (%s)\n", yytext); break;
+            case KW_BOOL: printf("KW_BOOL (%s)\n", yytext); break;
+            case KW_IF: printf("KW_IF (%s)\n", yytext); break;
+            case KW_THEN: printf("KW_THEN (%s)\n", yytext); break;
+            case KW_ELSE: printf("KW_ELSE (%s)\n", yytext); break;
+            case KW_WHILE: printf("KW_WHILE (%s)\n", yytext); break;
+            case KW_LOOP: printf("KW_LOOP (%s)\n", yytext); break;
+            case KW_READ: printf("KW_READ (%s)\n", yytext); break;
+            case KW_PRINT: printf("KW_PRINT (%s)\n", yytext); break;
+            case KW_RETURN: printf("KW_RETURN (%s)\n", yytext); break;
+            case OPERATOR_LE: printf("OPERATOR_LE (%s)\n", yytext); break;
+            case OPERATOR_GE: printf("OPERATOR_GE (%s)\n", yytext); break;
+            case OPERATOR_EQ: printf("OPERATOR_EQ (%s)\n", yytext); break;
+            case OPERATOR_DIF: printf("OPERATOR_DIF (%s)\n", yytext); break;
+            case TK_IDENTIFIER: printf("TK_IDENTIFIER (%s)\n", yytext);break;
+            case LIT_INTEGER: printf("LIT_INTEGER (%s)\n", yytext); break;
+            case LIT_FLOAT: printf("LIT_FLOAT (%s)\n", yytext); break;
+            case LIT_TRUE: printf("LIT_TRUE (%s)\n", yytext); break;
+            case LIT_FALSE: printf("LIT_FALSE (%s)\n", yytext); break;
+            case LIT_CHAR: printf("LIT_CHAR (%s)\n", yytext); break;
+            case LIT_STRING: printf("LIT_STRING (%s)\n", yytext); break;
+            case TOKEN_ERROR: printf("TOKEN_ERROR (%s)\n", yytext); break;
             default: printf("Outro: %c\n", yytext[0]); break;
         }
 

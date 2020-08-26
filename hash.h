119540741330b70a848edc9fe25ee61f17f79633
Lando, Gabriel <gabriel.lando@hp.com>
2020-08-26 11:29:04 -0300

    Added docs and a lot of things

---
+++ hash.h
@@ -0,0 +1,15 @@
+#define HASH_SIZE 997
+#define STRING_SIZE 100
+
+typedef struct hash_node {
+    char * text;
+    struct hash_node * next;
+
+}
+HASH_NODE;
+
+void hashInit(void);
+int hashAddress(char * text);
+HASH_NODE * hashInsert(char * text);
+HASH_NODE * hashFind(char * text);
+void hashPrint(void);

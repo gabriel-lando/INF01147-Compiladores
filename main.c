777e0bdf7d13cc33912ec984352ea992d9ed31ed
Lando, Gabriel <gabriel.lando@hp.com>
2020-08-26 12:06:24 -0300

    Changed main

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "tokens.h"
 
 //lex.yy.h
 int yylex();
@@ -24,23 +25,41 @@ int main(int argc, char ** argv) {
         printf("Cannot open file %s... \n", argv[1]);
         exit(1);
     }
-    if (0 == (gold = fopen(argv[2], "r"))) {
-        printf("Cannot open file %s... \n", argv[2]);
-        exit(1);
-    }
     while (isRunning()) {
         token = yylex();
 
         if (!isRunning())
             break;
-        fscanf(gold, "%d", & answar);
-        if (token == answar) {
-            fprintf(stderr, "%d=ok(%s)  ", i, yytext);
-            ++nota;
-        } else
-            fprintf(stderr, "\n%d=ERROR(%s,%d,%d) ", i, yytext, token, answar);
+
+        switch(token) {
+            case KW_CHAR: printf("Char\n"); break;
+            case KW_INT: printf("Int\n"); break;
+            case KW_FLOAT: printf("Float\n"); break;
+            case KW_BOOL: printf("Bool\n"); break;
+            case KW_IF: printf("If\n"); break;
+            case KW_THEN: printf("Then\n"); break;
+            case KW_ELSE: printf("Else\n"); break;
+            case KW_WHILE: printf("While\n"); break;
+            case KW_LOOP: printf("Loop\n"); break;
+            case KW_READ: printf("Read\n"); break;
+            case KW_PRINT: printf("Print\n"); break;
+            case KW_RETURN: printf("Return\n"); break;
+            case OPERATOR_LE: printf("OPERATOR_LE\n"); break;
+            case OPERATOR_GE: printf("OPERATOR_GE\n"); break;
+            case OPERATOR_EQ: printf("OPERATOR_EQ\n"); break;
+            case OPERATOR_DIF: printf("OPERATOR_DIF\n"); break;
+            case TK_IDENTIFIER: printf("TK_IDENTIFIER\n"); break;
+            case LIT_INTEGER: printf("LIT_INTEGER\n"); break;
+            case LIT_FLOAT: printf("LIT_FLOAT\n"); break;
+            case LIT_TRUE: printf("LIT_TRUE\n"); break;
+            case LIT_FALSE: printf("LIT_FALSE\n"); break;
+            case LIT_CHAR: printf("LIT_CHAR\n"); break;
+            case LIT_STRING: printf("LIT_STRING\n"); break;
+            case TOKEN_ERROR: printf("TOKEN_ERROR\n"); break;
+            default: printf("ERROR\n"); break;
+        }
+
+        fprintf(stderr, "%d=%d", i, yytext);
         ++i;
     }
-    printf("NOTA %d\n\n", nota);
-    fprintf(stderr, "NOTA %d\n\n", nota);
 }

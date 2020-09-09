6a03c510d619662945a32760d532b58276023e43
Gabriel Lando <gabriellando@hotmail.com>
2020-09-09 01:51:23 -0300

    [Etapa 2] Updated hash.c

--- Etapa2/hash.c
+++ Etapa2/hash.c
@@ -9,17 +9,15 @@ HASH_NODE * Table[HASH_SIZE];
 
 void hashInit(void) {
     int i;
-    for (i = 0; i < HASH_SIZE; ++i) {
+    for (i = 0; i < HASH_SIZE; ++i)
         Table[i] = 0;
-    }
 }
 
 int hashAddress(char * text) {
     int i;
     int address = 1;
-    for (i = 0; i < strlen(text); ++i) {
+    for (i = 0; i < strlen(text); ++i)
         address = (address * text[i]) % HASH_SIZE + 1;
-    }
     return address - 1;
 }
 
@@ -29,10 +27,10 @@ HASH_NODE * hashInsert(char * text, int type) {
   if ((newnode = hashFind(text)) != 0)
     return newnode;
   newnode = (HASH_NODE * ) calloc(1, sizeof(HASH_NODE));
-  newnode -> type = type;
-  newnode -> text = (char * ) calloc(strlen(text) + 1, sizeof(char));
+  newnode->type = type;
+  newnode->text = (char * ) calloc(strlen(text) + 1, sizeof(char));
   strcpy(newnode -> text, text);
-  newnode -> next = Table[address];
+  newnode->next = Table[address];
   Table[address] = newnode;
   return newnode;
 }
@@ -42,12 +40,9 @@ HASH_NODE * hashFind(char * text) {
     int address;
     address = hashAddress(text);
 
-    for (node = Table[address]; node; node = node -> next) {
-        if (!strcmp(text, node -> text)) {
+    for (node = Table[address]; node; node = node->next)
+        if (!strcmp(text, node -> text))
             return node;
-        }
-    }
-
     return 0;
 }
 
@@ -55,9 +50,7 @@ void hashPrint(void) {
     HASH_NODE * node;
 
     int i;
-    for (i = 0; i < HASH_SIZE; ++i) {
-        for (node = Table[i]; node; node = node -> next) {
-            fprintf(stderr, "Table [%3d]: %s \n", i, node -> text);
-        }
-    }
+    for (i = 0; i < HASH_SIZE; ++i)
+        for (node = Table[i]; node; node = node->next)
+            fprintf(stderr, "Table [%3d]: [Type %d] %s \n", i, node->type, node->text);
 }
